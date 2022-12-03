c41f64b0a87da9c8a28492d9f072eb617aa34ef9
Yannick <yannick.wesseloh@uni-konstanz.de>
2022-12-03 14:46:25 +0100

    Add hit threshold and points to TargetManager

--- Assets/Scripts/TargetManager.cs
+++ Assets/Scripts/TargetManager.cs
@@ -12,18 +12,23 @@ public class TargetManager : MonoBehaviour
 {
     public TargetState state = TargetState.Up;
     public ScoreboardManager scoreboardManager;
+    public int hitThreshold = 1;
+    public int points = 1;
 
     private HingeJoint targetJoint;
+    private Rigidbody rb;
 
     void Start()
     {
         targetJoint = GetComponent<HingeJoint>();
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         if(transform.localRotation.eulerAngles.x < 0.1) {
             targetJoint.useSpring = false;
+            rb.isKinematic = hitThreshold > 1;
             state = TargetState.Up;
         } else if(transform.localRotation.eulerAngles.x > 89.9) {
             targetJoint.useMotor = false;
@@ -33,9 +38,15 @@ public class TargetManager : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(state == TargetState.Up) {
+        if(collision.gameObject.tag != "Bullet" || state != TargetState.Up) { return; }
+
+        BulletManager bulletManager = collision.gameObject.GetComponent<BulletManager>();
+        if(bulletManager.collisionCount > 1) { return; }
+
+        hitThreshold -= 1;
+        if(hitThreshold == 0) {
             state = TargetState.Hit;
-            scoreboardManager.UpdateScore(1);
+            scoreboardManager.UpdateScore(points);
         }
     }
 
